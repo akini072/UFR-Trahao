3c060bad9d450e68c78c8732f4b840757efb8e2f
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-08 21:05:09 -0300

    feat: application configured theme + added tailwind forms plugin to styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,34 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent: 'transparent',
+      success: "#22c55e",
+      alert: "#fbbf24",
+      error: "#b91c1c",
+      "default-white": "#f1f5f9",
+      "default-black": "#1a202c",
+    },
+    fontFamily:{
+      sans: ['Montserrat', 'sans-serif'],
+      serif: ['Roboto', 'serif'],
+    },
+    extend: {
+      spacing:{
+        "100": "30rem",
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    })
+  ],
 }
 
