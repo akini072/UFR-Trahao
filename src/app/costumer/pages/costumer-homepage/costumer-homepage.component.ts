590b948b442e42887567a00875f68ee65beb6079
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-10 20:46:30 -0300

    wip: navbar component

--- src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
+++ src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../core/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-costumer-homepage',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './costumer-homepage.component.html',
   styleUrl: './costumer-homepage.component.css'
 })
-export class TelaHomecostumerComponent {
+export class CustomerHomepageComponent {
 
 }
