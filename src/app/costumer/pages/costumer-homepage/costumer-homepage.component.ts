c16cd0a40b9fc82d82a1a34f77af61a2a0c25eb3
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-11 19:32:42 -0300

    feat: navbar component --todo routing navigation

--- src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
+++ src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../core/components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-costumer-homepage',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, NavbarComponent, RouterModule],
   templateUrl: './costumer-homepage.component.html',
   styleUrl: './costumer-homepage.component.css'
 })
