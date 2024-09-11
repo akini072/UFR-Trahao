4287709165a61c69f630a817033892ab53e8f3b8
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-11 19:42:06 -0300

    refactor: navbar classes

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -14,10 +14,13 @@ interface NavItem {
   styleUrls: ['./navbar.component.css'] // Corrected to styleUrls
 })
 export class NavbarComponent {
-  navContainer = "flex flex-1 bg-primary-3 h-16 justify-end shadow-md";
+  navContainer = "flex flex-1 bg-primary-3 h-16 justify-between shadow-md";
   navItem = "flex h-full px-2 hover:bg-primary-4 cursor-pointer text-center align-middle items-center justify-center";
   navList = "flex justify-end gap-2 h-full";
-  navDiv = "flex flex-1 items-center px-6 h-full justify-end";
+  navDiv = "flex flex-1 items-center px-6 h-full justify-between";
+  navLogo = "px-4 text-3xl font-serif text-secondary-8"
+  navUserText = "text-secondary-0";
+  navData = "flex gap-4 align-middle justify-center items-center";
 
   navItems: NavItem[] = [
     { name: 'Home', route: '/index' },
