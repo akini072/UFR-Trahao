5c8d07893bced4dab3088ef3f20dea8e8caa08e2
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-11 22:56:43 -0300

    feature/create the status button

--- src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
+++ src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
@@ -3,11 +3,11 @@ import { NavbarComponent } from "../../../core/components/navbar/navbar.componen
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ServiceRequestTableComponent } from '../../components/service-request-table/service-request-table.component';
-
+import { ButtonStatusComponent } from '../../../button-status/button-status.component';
 @Component({
   selector: 'app-costumer-homepage',
   standalone: true,
-  imports: [CommonModule, NavbarComponent,ServiceRequestTableComponent, RouterModule],
+  imports: [CommonModule, NavbarComponent,ServiceRequestTableComponent, RouterModule, ButtonStatusComponent],
   templateUrl: './costumer-homepage.component.html',
   styleUrl: './costumer-homepage.component.css'
 })
