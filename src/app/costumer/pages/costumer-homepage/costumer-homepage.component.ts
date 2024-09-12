445d79b961db6ce3a676bf878c5e4900a5506da7
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-11 21:03:19 -0300

    wip: request service table

--- src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
+++ src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../core/components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ServiceRequestTableComponent } from '../../components/service-request-table/service-request-table.component';
 
 @Component({
   selector: 'app-costumer-homepage',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, RouterModule],
+  imports: [CommonModule, NavbarComponent,ServiceRequestTableComponent, RouterModule],
   templateUrl: './costumer-homepage.component.html',
   styleUrl: './costumer-homepage.component.css'
 })
