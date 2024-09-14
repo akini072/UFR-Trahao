15383666fad6318161fbd344a787296ef48f2337
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-14 00:07:19 -0300

    wip: refactor navbar

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 interface NavItem {
   name: string;
   route: string;
+  login?: boolean;
 }
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule], // Add CommonModule to imports array
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'] // Corrected to styleUrls
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
-  navContainer = "flex flex-1 bg-primary-8 h-16 justify-between shadow-md";
+  @Input() isLogged: boolean = true;
+
+  navContainer = "flex flex-1 bg-primary-8 h-16 justify-between";
   navItem = "flex h-full px-2 hover:bg-primary-7 cursor-pointer text-center align-middle items-center justify-center";
   navList = "flex justify-end gap-2 h-full";
   navDiv = "flex flex-1 items-center px-6 h-full justify-between";
@@ -23,18 +26,10 @@ export class NavbarComponent {
   navData = "flex gap-4 align-middle justify-center items-center";
 
   navItems: NavItem[] = [
-    { name: 'Home', route: '/index' },
     { name: 'Login', route: '/login' },
     { name: 'Cadastro', route: '/cadastro' },
-    { name: 'Customer', route: '/customer' },
-    { name: 'Serviços', route: '/services' }
+    { name: 'Home', route: '/home', login: true },
+    { name: 'Serviços', route: '/about', login: true }
   ];
-
-  shouldDisplay(item: NavItem): boolean {
-    // Add your conditional logic here
-    // For example, only display items with the route '/index' or '/login'
-    return item.route === '/index' || item.route === '/login';
-  }
-
   
 }
