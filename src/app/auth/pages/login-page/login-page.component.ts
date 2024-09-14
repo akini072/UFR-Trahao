f40393be81e70edd604510c3f1380d994d10e0ed
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-14 15:54:06 -0300

    feat: applying form text input component to login page

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../core/components/navbar/navbar.component';
+import { FormTextInputComponent } from '../../../core/components/form-text-input/form-text-input.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../../../core/components/footer/footer.component";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, FormTextInputComponent, FooterComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
