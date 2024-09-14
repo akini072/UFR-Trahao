fee972827403f889a53024b7fdda82c1bd0ed7ed
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-14 20:41:11 -0300

    feat: create remaining chars logic

--- src/app/costumer/pages/new-request-page/new-request-page.component.ts
+++ src/app/costumer/pages/new-request-page/new-request-page.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../../../core/components/navbar/navbar.component";
 import { FooterComponent } from "../../../core/components/footer/footer.component";
 
 @Component({
   selector: 'app-new-request-page',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './new-request-page.component.html',
   styleUrl: './new-request-page.component.css'
 })
 export class NewRequestPageComponent {
+  remainingCharactersEquipamento = 30;
+  remainingCharactersDefeito = 200;
 
+  getRemainingCharacters(limit: number, inputId: string): void {
+    const inputElement = document.getElementById(inputId) as HTMLInputElement | HTMLTextAreaElement;
+    if (inputElement) {
+      const currentLength = inputElement.value.length;
+      if (inputId === 'descricaoEquipamento') {
+        this.remainingCharactersEquipamento = limit - currentLength;
+      } else if (inputId === 'descricaoDefeito') {
+        this.remainingCharactersDefeito = limit - currentLength;
+      }
+    }
+  }
 }
