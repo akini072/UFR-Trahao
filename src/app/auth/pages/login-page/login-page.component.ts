e55b4d2621660ec7589ec5db79c56a06a890ed7f
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-14 01:07:09 -0300

    feat: base login-page

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../core/components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
