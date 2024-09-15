7237932b7f506d7628955c7af72a22f6c8d28524
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-14 23:20:45 -0300

    chore: add comments for new request page

--- src/app/costumer/pages/new-request-page/new-request-page.component.ts
+++ src/app/costumer/pages/new-request-page/new-request-page.component.ts
@@ -11,9 +11,17 @@ import { FooterComponent } from "../../../core/components/footer/footer.componen
   styleUrl: './new-request-page.component.css'
 })
 export class NewRequestPageComponent {
-  remainingCharactersEquipamento = 30;
-  remainingCharactersDefeito = 255;
+  remainingCharactersEquipamento = 30; //Caracteres restantes para atingir o limite no input de descrição do equipamento
+  remainingCharactersDefeito = 255; //Caracteres restantes para atingir o limite no input de descrição do defeito
 
+  /**
+ * Atualiza a quantidade de caracteres restantes em um input/textarea cada vez que o conteúdo do campo é alterado
+ * 
+ * @param limit - O número máximo de caracteres permitidos
+ * @param inputId - O ID do elemento de entrada (input ou textarea)
+ * 
+ * @returns - não retorna nenhum valor
+ */
   getRemainingCharacters(limit: number, inputId: string): void {
     const inputElement = document.getElementById(inputId) as HTMLInputElement | HTMLTextAreaElement;
     if (inputElement) {
