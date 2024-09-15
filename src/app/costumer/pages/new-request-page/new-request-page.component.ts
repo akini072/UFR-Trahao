ec20ba4f3a7197fd6a68257a176bb3263c4c7ae5
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-14 22:51:12 -0300

    fix: standardize HTML class names and adjust input character limits

--- src/app/costumer/pages/new-request-page/new-request-page.component.ts
+++ src/app/costumer/pages/new-request-page/new-request-page.component.ts
@@ -12,15 +12,15 @@ import { FooterComponent } from "../../../core/components/footer/footer.componen
 })
 export class NewRequestPageComponent {
   remainingCharactersEquipamento = 30;
-  remainingCharactersDefeito = 200;
+  remainingCharactersDefeito = 255;
 
   getRemainingCharacters(limit: number, inputId: string): void {
     const inputElement = document.getElementById(inputId) as HTMLInputElement | HTMLTextAreaElement;
     if (inputElement) {
       const currentLength = inputElement.value.length;
-      if (inputId === 'descricaoEquipamento') {
+      if (inputId === 'descricao-equipamento') {
         this.remainingCharactersEquipamento = limit - currentLength;
-      } else if (inputId === 'descricaoDefeito') {
+      } else if (inputId === 'descricao-defeito') {
         this.remainingCharactersDefeito = limit - currentLength;
       }
     }
