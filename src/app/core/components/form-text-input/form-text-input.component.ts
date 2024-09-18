faba837ff20ae18e00d0f6c1af732f59576f3033
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-18 17:19:16 -0300

    fix: refactoring the form text input component

--- src/app/core/components/form-text-input/form-text-input.component.ts
+++ src/app/core/components/form-text-input/form-text-input.component.ts
@@ -1,40 +1,56 @@
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
-interface FormTextInput {
-  status?: string;
 
-}
+type FormTextInputType = 'text' | 'password' | 'email' | 'number' | 'date';
 
 @Component({
   selector: 'app-form-text-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form-text-input.component.html',
-  styleUrl: './form-text-input.component.css'
+  styleUrls: ['./form-text-input.component.css'],
+  providers: [DatePipe]
 })
+
 export class FormTextInputComponent {
-  @Input() type: string | undefined;
-  @Input() status: string | undefined;
+  @Input() type: FormTextInputType = "text";
   @Input() placeholder: string | undefined;
+  @Input() value: string | undefined;
+  formattedDate: string | undefined;
 
   input = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
-  intputPassword = `${this.input} `;
+  inputPassword = `${this.input} `;
+  inputDate = `${this.input} `;
 
-  getInputTextClass(status: string | undefined): string {
-    let inputTextClass : string;
-    switch (status) {
-      case 'senha':
-        inputTextClass =  this.intputPassword;
+  constructor(private datePipe: DatePipe) { }
+
+  ngOnInit(): void {
+    if (this.type === 'date' && this.value) {
+      this.formattedDate = this.datePipe.transform(this.value, 'dd-MM-yyyy')!;
+    }
+  }
+
+  getInputTextClass(type: FormTextInputType | undefined): string {
+    let inputTextClass: string;
+    switch (type) {
+      case 'text':
+        inputTextClass = this.inputPassword;
+        break;
+      case 'password':
+        inputTextClass = this.inputPassword;
+        break;
+      case 'email':
+        inputTextClass = this.input;
+        break;
+      case 'number':
+        inputTextClass = this.input;
         break;
       default:
-        inputTextClass =  this.input;
+        inputTextClass = this.input;
         break;
     }
     return inputTextClass;
   }
 
-  formTextInput: FormTextInput[] = [
-    { status: 'valid' },
-    { status: 'invalid' }
-  ];
 }
