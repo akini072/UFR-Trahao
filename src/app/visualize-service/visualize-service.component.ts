82eb64ad91a41385f8cf530978ba3b608d9a10e8
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-18 20:41:06 -0300

    feat: add visualize service

---
+++ src/app/visualize-service/visualize-service.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualize-service',
+  standalone: true,
+  imports: [],
+  templateUrl: './visualize-service.component.html',
+  styleUrl: './visualize-service.component.css'
+})
+export class VisualizeServiceComponent {
+
+}
