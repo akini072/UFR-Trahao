09a799a3a7c7cdb53583219677410e9ec51ea795
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-18 22:11:52 -0300

    wip: visualize request

--- src/app/visualize-service/visualize-service.component.ts
+++ src/app/visualize-service/visualize-service.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../core/components/navbar/navbar.component';
+import { FooterComponent } from '../core/components/footer/footer.component';
+import { ButtonComponent } from '../core/components/button/button.component';
 
 @Component({
   selector: 'app-visualize-service',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent, ButtonComponent, CommonModule],
   templateUrl: './visualize-service.component.html',
   styleUrl: './visualize-service.component.css'
 })
 export class VisualizeServiceComponent {
 
+  onLogin(){
+    //TODO:
+
+  }
+
+  button: string = "flex items-center justify-between mb-4 flex md:justify-center";
 }
