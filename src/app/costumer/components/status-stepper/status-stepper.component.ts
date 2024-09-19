bc843a5696cc9b90d7f99e2c44fce5272882592d
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-18 23:53:22 -0300

    wip: basic status-stepper made

--- src/app/costumer/components/status-stepper/status-stepper.component.ts
+++ src/app/costumer/components/status-stepper/status-stepper.component.ts
@@ -15,6 +15,7 @@ export class StatusStepperComponent {
   liStart : string = "flex w-full relative text-indigo-600  after:content-['']  after:w-full after:h-0.5  after:bg-indigo-600 after:inline-block after:absolute lg:after:top-5 after:top-3 after:left-4";
   liMiddle : string = "flex w-full relative text-gray-900  after:content-['']  after:w-full after:h-0.5  after:bg-gray-200 after:inline-block after:absolute lg:after:top-5 after:top-3 after:left-4";
   item : string = "block whitespace-nowrap z-10";
+  complete : string = "w-6 h-6 bg-indigo-600 border-2 border-indigo-600 rounded-full flex justify-center items-center mx-auto mb-3 text-sm text-white lg:w-10 lg:h-10";
   wip: string = "w-6 h-6 bg-indigo-50 border-2 border-indigo-600 rounded-full flex justify-center items-center mx-auto mb-3 text-sm text-indigo-600 lg:w-10 lg:h-10";
   incomplete : string = "w-6 h-6 bg-gray-50 border-2 border-gray-200 rounded-full flex justify-center items-center mx-auto mb-3 text-sm  lg:w-10 lg:h-10";
 }
