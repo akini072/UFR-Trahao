e8cb6632341c3d54a224568f9a15640741728010
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-18 23:49:26 -0300

    wip: basic status-stepper

---
+++ src/app/costumer/components/status-stepper/status-stepper.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-stepper',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './status-stepper.component.html',
+  styleUrl: './status-stepper.component.css'
+})
+export class StatusStepperComponent {
+
+
+  ol : string = "flex items-center w-full text-xs text-gray-900 font-medium sm:text-base";
+  liStart : string = "flex w-full relative text-indigo-600  after:content-['']  after:w-full after:h-0.5  after:bg-indigo-600 after:inline-block after:absolute lg:after:top-5 after:top-3 after:left-4";
+  liMiddle : string = "flex w-full relative text-gray-900  after:content-['']  after:w-full after:h-0.5  after:bg-gray-200 after:inline-block after:absolute lg:after:top-5 after:top-3 after:left-4";
+  item : string = "block whitespace-nowrap z-10";
+  wip: string = "w-6 h-6 bg-indigo-50 border-2 border-indigo-600 rounded-full flex justify-center items-center mx-auto mb-3 text-sm text-indigo-600 lg:w-10 lg:h-10";
+  incomplete : string = "w-6 h-6 bg-gray-50 border-2 border-gray-200 rounded-full flex justify-center items-center mx-auto mb-3 text-sm  lg:w-10 lg:h-10";
+}
