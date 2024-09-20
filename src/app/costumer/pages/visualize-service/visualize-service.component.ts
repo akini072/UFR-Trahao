746eda34d5646c1236720016eac1c20b36bb542c
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-19 23:32:55 -0300

    feat: conditional page

--- src/app/costumer/pages/visualize-service/visualize-service.component.ts
+++ src/app/costumer/pages/visualize-service/visualize-service.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../../core/components/navbar/navbar.component';
 import { FooterComponent } from '../../../core/components/footer/footer.component';
 import { ButtonComponent } from '../../../core/components/button/button.component';
 import { StatusStepperComponent } from '../../components/status-stepper/status-stepper.component';
+import { Request } from '../../../core/types/request';
+
 
 @Component({
   selector: 'app-visualize-service',
@@ -12,17 +14,58 @@ import { StatusStepperComponent } from '../../components/status-stepper/status-s
   templateUrl: './visualize-service.component.html',
   styleUrl: './visualize-service.component.css'
 })
-export class VisualizeServiceComponent {
+
+
+
+export class VisualizeServiceComponent implements OnChanges, OnInit {
+
+  budgeted: boolean = false;
+  finalized: boolean = false;
+
+  ngOnInit(): void {
+      this.checkStatus();
+  }
+
+  requestList: Request[] = [
+    {
+      id: 1,
+      title: 'Alisson da Silva',
+      description: 'Computador',
+      status: 'pending',
+      created_at: '',
+      image: ''
+    }
+  ];
+
+  ngOnChanges(): void {
+    console.log("Valor mudasse")
+  }
   onReject(){
   }
+
   onApprove(){
   }
 
   onPay(){
   }
 
-  budgeted = false;
-  finalized = false;
+  checkStatus(){
+    const request = this.requestList[0];
 
+    switch (request.status){
+      case 'approved':
+        this.budgeted = false;
+        this.finalized = true;
+        break;
+      case 'pending':
+        this.budgeted = true;
+        this.finalized = false;
+        break;
+      default:
+        this.budgeted = false;
+        this.finalized = false;
+        break;
+    }
+  }
   button: string = "flex items-center justify-between mb-4 flex md:justify-center";
 }
