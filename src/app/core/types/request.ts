9fd6a78f442da5154e811f4997b31f70b816e1f5
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-21 21:43:25 -0300

    wip: pagination

--- src/app/core/types/request.ts
+++ src/app/core/types/request.ts
@@ -1,4 +1,19 @@
-export type RequestStatus = 'pending' | 'approved' | 'rejected' | 'paid' | "canceled" |  undefined;
+/* ORÇADA: Botão Aprovar/Rejeitar Serviço, que apresenta a tela de Mostrar orçamento FED - RF005 - Tela de Mostrar orçamento #5
+APROVADA: Sem botão de ação
+REJEITADA: Botão Resgatar Serviço FED - RF009 - Modal Resgatar Serviço #9
+ARRUMADA: Botão Pagar Serviço FED - RF010 - Tela Pagar Serviço #10
+Outros estados: Botão Visualizar Serviço FED - RF008 - Tela Visualizar Serviço #8 */
+
+/* + OPEN
++ BUDGETED
++ REJECTED
++ APPROVED
++ REDIRECTED
++ FIXED
++ PAID
++ FINALIZED */
+
+export type RequestStatus = 'open' | "budgeted" | 'approved' | 'rejected' | "fixed" | 'paid' | "finalized" | "redirected" | undefined;
 
 export interface Request {
     id: number;
