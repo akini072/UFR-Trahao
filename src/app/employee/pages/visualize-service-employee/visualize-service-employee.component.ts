a24c9221a6641d9b673342552f65281c1ffb9c35
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-23 19:03:04 -0300

    feat: create the employee page

---
+++ src/app/employee/pages/visualize-service-employee/visualize-service-employee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualize-service-employee',
+  standalone: true,
+  imports: [],
+  templateUrl: './visualize-service-employee.component.html',
+  styleUrl: './visualize-service-employee.component.css'
+})
+export class VisualizeServiceEmployeeComponent {
+
+}
