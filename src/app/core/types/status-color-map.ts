e61a29718bf6314dca23207aaa48a6ee03de09d5
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-23 17:42:21 -0300

    feat: status color map

---
+++ src/app/core/types/status-color-map.ts
@@ -0,0 +1,10 @@
+export const statusColorMap: { [key: string]: string } = {
+  'open': 'gray-400',
+  'budgeted': 'amber-900',
+  'rejected': 'red-500',
+  'approved': 'amber-500',
+  'redirected': 'violet-700',
+  'fixed': 'blue-600',
+  'paid': 'orange-500',
+  'finalized': 'green-600'
+}
