6bbb291cae9bf3c13f474cc46c530e2f70042482
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-24 18:48:23 -0300

    feat: flowbite instalation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,10 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'FED-WEB2-UFPR-Trabalho-Final';
+
+  ngOnInit() {
+    initFlowbite();
+  }
 }
