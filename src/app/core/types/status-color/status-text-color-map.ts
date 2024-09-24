c8649412aeb449a0a06e7fbcdb596fd08b318eab
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-24 00:28:53 -0300

    feat: map colors by category

---
+++ src/app/core/types/status-color/status-text-color-map.ts
@@ -0,0 +1,10 @@
+export const statusTextColor: { [key: string]: string } = {
+  "open": "text-gray-400",
+  "budgeted": "text-amber-900",
+  "rejected": "text-red-500",
+  "approved": "text-amber-500",
+  "redirected": "text-violet-700",
+  "fixed": "text-blue-600",
+  "paid": "text-orange-500",
+  "finalized": "text-green-600"
+}
