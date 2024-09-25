9fe637c8a312801c379fb2d68e704d58cb3de2ae
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-24 23:56:52 -0300

    wip: employee service page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginPageComponent } from './auth/pages/login-page/login-page.component
 import { SignupPageComponent } from './auth/pages/signup-page/signup-page.component';
 import { CustomerHomepageComponent } from './costumer/pages/costumer-homepage/costumer-homepage.component';
 import { NewRequestPageComponent } from './costumer/pages/new-request-page/new-request-page.component';
+import { VisualizeServiceEmployeeComponent } from './employee/pages/visualize-service-employee/visualize-service-employee.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/index', pathMatch: 'full' },
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: 'login', component: LoginPageComponent },
     { path: 'cadastro', component: SignupPageComponent },
     { path: 'customer', component: CustomerHomepageComponent },
-    { path: 'nova-solicitacao', component: NewRequestPageComponent }
+    { path: 'nova-solicitacao', component: NewRequestPageComponent },
+    { path: 'visualizar-empregado', component: VisualizeServiceEmployeeComponent}
 ];
--- src/app/employee/pages/visualize-service-employee/visualize-service-employee.component.ts
+++ src/app/employee/pages/visualize-service-employee/visualize-service-employee.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../core/components/button/button.component';
 
 @Component({
   selector: 'app-visualize-service-employee',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './visualize-service-employee.component.html',
   styleUrl: './visualize-service-employee.component.css'
 })
 export class VisualizeServiceEmployeeComponent {
 
+  onFix(){}
+    //implementar a lógica para arrumar o componente. deve guardar o funcionario, data e hora
+  ;
+
+  onRedirect(){
+    //implementar a lógica para passar o concerto para outro funcionario
+  };
+
+  onBudget(){
+    //implementar lógica de salvar o valor do orçamento e timestamp junto do nome do funcionario
+  };
 }
