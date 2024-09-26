4f58d4e84e3c20c126a81b451a4e83b97ed9a206
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-26 19:08:54 -0300

    feat: custom status pipes

---
+++ src/app/core/utils/pipes/statusText/status-text.pipe.ts
@@ -0,0 +1,33 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { RequestCategory } from '../../../types/request-category';
+
+@Pipe({
+  name: 'statusText',
+  standalone: true
+})
+export class StatusTextPipe implements PipeTransform {
+
+  transform(value: RequestCategory): string {
+    switch(value) {
+      case 'open':
+        return 'Aberta';
+      case 'budgeted':
+        return 'Orçada';
+      case 'rejected':
+        return 'Rejeitada';
+      case 'approved':
+        return 'Aprovada';
+      case 'redirected':
+        return 'Redirecionada';
+      case 'fixed':
+        return 'Arrumada';
+      case 'paid':
+        return 'Paga';
+      case 'finalized':
+        return 'Finalizada';
+      default:
+        return 'Indefinido';
+    }
+  }
+
+}
