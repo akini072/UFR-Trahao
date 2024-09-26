07eed3143755080fb4bcaa5a097d2082f0827b85
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-26 19:16:11 -0300

    fix: stop showing employee in the approved popover

--- src/app/costumer/components/status-stepper/status-stepper.component.ts
+++ src/app/costumer/components/status-stepper/status-stepper.component.ts
@@ -104,13 +104,14 @@ export class StatusStepperComponent implements OnInit {
   private getCompletePopoverText(category: string, index: number, dateTimeText: string): string {
     const statusText = statusMap[category];
     const senderEmployee = this.statusList[index].senderEmployee;
+    console.log(senderEmployee);
     const inChargeEmployee = this.statusList[index].inChargeEmployee;
 
     if (category === 'redirected') {
       return `${statusText} por ${senderEmployee} para ${inChargeEmployee} em ${dateTimeText}`;
     }
 
-    if (category !== 'open' && category !== 'paid' && category !== 'rejected') {
+    if (category !== 'open' && category !== 'paid' && category !== 'rejected' && category !== 'approved') {
       return `${statusText} por ${inChargeEmployee} em ${dateTimeText}`;
     }
 
