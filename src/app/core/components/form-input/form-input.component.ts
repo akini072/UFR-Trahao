5d3c7d653d383a2363bcb58a74d14de62f1aaff8
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-27 16:55:06 -0300

    refactor: improve html structure and include formControls

--- src/app/core/components/form-input/form-input.component.ts
+++ src/app/core/components/form-input/form-input.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule, DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 
 type FormTextInputType = 'text' | 'password' | 'email' | 'number' | 'date';
@@ -7,33 +8,23 @@ type FormTextInputType = 'text' | 'password' | 'email' | 'number' | 'date';
 @Component({
   selector: 'app-form-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-input.component.html',
-  styleUrls: ['./form-input.component.css'],
-  providers: [DatePipe]
+  styleUrls: ['./form-input.component.css']
 })
 
 export class FormInputComponent {
   @Input() type: FormTextInputType = 'text';
-  @Input() placeholder: string | undefined;
-  @Input() value: string | undefined;
-  formattedDate: string | undefined;
-  input = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
-  eye: string = "absolute inset-y-0 end-0 flex items-center z-20 px-3 cursor-pointer text-gray-400 rounded-e-md focus:outline-none focus:text-blue-600 dark:text-neutral-600 dark:focus:text-blue-500";
+  @Input() placeholder?: string;
+  @Input() control?: FormControl;
+  input = "shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline";
+  eye: string = "absolute inset-y-0 end-0 flex items-center z-20 px-3 cursor-pointer text-gray-400 rounded-e-md focus:outline-none hover:text-blue-600 dark:text-neutral-600 dark:hover:text-blue-500";
   icon: string = 'visibility_off';
   visibility: boolean = false;
 
-  constructor(private datePipe: DatePipe) { }
-
-  ngOnInit(): void {
-    if (this.type === 'date' && this.value) {
-      this.formattedDate = this.datePipe.transform(this.value, 'dd-MM-yyyy')!;
-    }
-  }
-
   toggleVisibility(): void {
-    this.visibility = !this.visibility;
     this.icon = this.visibility ? `visibility_off` : `visibility`;
+    this.visibility = !this.visibility;
     this.type = this.visibility ? 'text' : 'password';
   }
 }
