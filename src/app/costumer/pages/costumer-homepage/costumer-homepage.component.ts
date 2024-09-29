c0c9c2cd949175b0367561a5eabeadfd89ff8ae1
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-09-29 19:01:17 -0300

    feat: filters + colors refactir

--- src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
+++ src/app/costumer/pages/costumer-homepage/costumer-homepage.component.ts
@@ -4,10 +4,15 @@ import { Router, RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../../core/components/navbar/navbar.component';
 import { ServiceRequestTableComponent } from '../../components/service-request-table/service-request-table.component';
 import { RequestCardComponent } from '../../components/request-card/request-card.component';
-import { ButtonComponent, ButtonProps } from "../../../core/components/button/button.component";
-import { FormTextInputComponent } from "../../../core/components/form-text-input/form-text-input.component";
+import {
+  ButtonComponent,
+  ButtonProps,
+} from '../../../core/components/button/button.component';
+import { FormTextInputComponent } from '../../../core/components/form-text-input/form-text-input.component';
 import { RequestCategory } from '../../../core/types/request-category';
 import { RequestTableComponent } from '../../components/request-table/request-table.component';
+import { FilterSectionComponent } from '../../components/filter-section/filter-section.component';
+import { ToggleSwitchComponent } from '../../../core/components/toggle-switch/toggle-switch.component';
 export interface RequestItem {
   id: number;
   title: string;
@@ -20,31 +25,42 @@ export interface RequestItem {
 @Component({
   selector: 'app-costumer-homepage',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, ServiceRequestTableComponent, RouterModule,RequestCardComponent, ButtonComponent, FormTextInputComponent, RequestTableComponent],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    ServiceRequestTableComponent,
+    RouterModule,
+    RequestCardComponent,
+    ButtonComponent,
+    FormTextInputComponent,
+    RequestTableComponent,
+    FilterSectionComponent,
+    ToggleSwitchComponent,
+  ],
   templateUrl: './costumer-homepage.component.html',
-  styleUrls: ['./costumer-homepage.component.css']
+  styleUrls: ['./costumer-homepage.component.css'],
 })
-
-
-
 export class CustomerHomepageComponent implements OnInit, OnDestroy {
-
   requestList: RequestItem[] = [
     {
       id: 1,
       title: 'Troca de Disco Rígido',
-      description: 'Substituição do disco rígido antigo por um novo SSD para melhorar o desempenho do sistema.',
+      description:
+        'Substituição do disco rígido antigo por um novo SSD para melhorar o desempenho do sistema.',
       status: 'open',
       created_at: '2024-09-01',
-      image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
+      image:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
     },
     {
       id: 2,
       title: 'Atualização de Software',
-      description: 'Atualização do sistema operacional e dos principais aplicativos para a versão mais recente.',
+      description:
+        'Atualização do sistema operacional e dos principais aplicativos para a versão mais recente.',
       status: 'approved',
       created_at: '2024-09-02',
-      image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
+      image:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
     },
     {
       id: 3,
@@ -52,31 +68,38 @@ export class CustomerHomepageComponent implements OnInit, OnDestroy {
       description: 'Reparo da tela quebrada do notebook.',
       status: 'rejected',
       created_at: '2024-09-03',
-      image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
+      image:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
     },
     {
       id: 4,
       title: 'Limpeza de Sistema',
-      description: 'Limpeza completa do sistema para remover poeira e melhorar a ventilação.',
+      description:
+        'Limpeza completa do sistema para remover poeira e melhorar a ventilação.',
       status: 'paid',
       created_at: '2024-09-04',
-      image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
+      image:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
     },
     {
       id: 5,
       title: 'Instalação de Memória RAM',
-      description: 'Instalação de memória RAM adicional para melhorar o desempenho do computador.',
+      description:
+        'Instalação de memória RAM adicional para melhorar o desempenho do computador.',
       status: 'budgeted',
       created_at: '2024-09-05',
-      image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
+      image:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
     },
     {
       id: 6,
       title: 'Configuração de Rede',
-      description: 'Configuração de rede local e Wi-Fi para melhor conectividade.',
+      description:
+        'Configuração de rede local e Wi-Fi para melhor conectividade.',
       status: 'finalized',
       created_at: '2024-09-06',
-      image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
+      image:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
     },
     {
       id: 7,
@@ -84,16 +107,19 @@ export class CustomerHomepageComponent implements OnInit, OnDestroy {
       description: 'Diagnóstico e reparo da placa mãe do desktop.',
       status: 'fixed',
       created_at: '2024-09-07',
-      image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
+      image:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
     },
     {
       id: 8,
       title: 'Substituição de Fonte de Alimentação',
-      description: 'Substituição da fonte de alimentação defeituosa por uma nova.',
+      description:
+        'Substituição da fonte de alimentação defeituosa por uma nova.',
       status: 'redirected',
       created_at: '2024-09-08',
-      image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
-    }
+      image:
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
+    },
   ];
 
   style = {
@@ -102,20 +128,26 @@ export class CustomerHomepageComponent implements OnInit, OnDestroy {
     container: 'flex w-full px-4 my-8 mx-auto',
     innerContainer: 'flex justify-end  gap-4',
     searchContainer: 'flex gap-2',
-    requestGrid: 'grid grid-cols-1 w-10/12 m-auto justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-4 p-4',
-    paginationControl: 'w-10/12 m-auto flex justify-end my-4 items-center text-center',
+    requestGrid:
+      'grid grid-cols-1 w-10/12 m-auto justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-4 p-4',
+    paginationControl:
+      'w-10/12 m-auto flex justify-end my-4 items-center text-center',
     pageText: 'border p-2 text-sm',
-    pageTopContainer: "flex justify-between w-full items-center px-16"
+    pageTopContainer: 'flex justify-between w-full items-center px-16',
+    tableDisplay: "flex justify-center m-auto rounded-lg w-3/4",
+    filterContainer: "flex place-items-end",
+    switchContainer: "h-8",
   };
 
-  activeRequestList: RequestItem[] = this.requestList
-
+  activeRequestList: RequestItem[] = this.requestList;
 
   currentPage: number = 1;
   itemsPerPage: number = 9;
   totalPages: number = 1;
   resizeListener!: () => void;
-  searchQuery: string | undefined
+  searchQuery: string | undefined;
+  activeFilters: { filter: string; value?: string }[] = [];
+  displayTable: boolean = false;
 
   constructor(private router: Router, private renderer: Renderer2) {
     this.updateTotalPages();
@@ -138,31 +170,38 @@ export class CustomerHomepageComponent implements OnInit, OnDestroy {
 
   updateItemsPerPage(width: number) {
     if (width >= 1200) {
-      this.itemsPerPage = 9; 
+      this.itemsPerPage = 9;
     } else if (width >= 992) {
-      this.itemsPerPage = 6; 
+      this.itemsPerPage = 6;
     } else {
-      this.itemsPerPage = 3; 
+      this.itemsPerPage = 3;
     }
-    this.updateTotalPages(); 
+    this.updateTotalPages();
   }
 
-  searchKeyboard = (event: Event) => {
-    const input = event.target as HTMLInputElement;
-    const query = input.value;
-  
-    this.searchQuery = query ? query.toLocaleLowerCase() : ""
- 
-    if(this.searchQuery === ""){
-      this.activeRequestList = this.requestList;
-      this.updateTotalPages();
-      this.goToPage(1);
-    }
+  toggleDisplayTable = () => {
+    this.displayTable = !this.displayTable;
+    console.log(this.displayTable);
+  };
+
+  searchKeyboard(event: Event): void {
+    const searchQuery = (event.target as HTMLInputElement).value.toLowerCase();
+    const filteredList = this.requestList.filter((item) => {
+      return this.activeFilters.every(
+        (f) => item[f.filter as keyof RequestItem] === f.value
+      );
+    });
+
+    this.activeRequestList = filteredList.filter((item) => {
+      return Object.values(item).some((val) =>
+        val.toString().toLowerCase().includes(searchQuery)
+      );
+    });
   }
 
   updateTotalPages() {
-    this.totalPages = Math.ceil(this.activeRequestList.length / this.itemsPerPage) || 1;
-
+    this.totalPages =
+      Math.ceil(this.activeRequestList.length / this.itemsPerPage) || 1;
   }
 
   getPaginatedRequests(searchQuery?: string): RequestItem[] {
@@ -172,7 +211,11 @@ export class CustomerHomepageComponent implements OnInit, OnDestroy {
     let list = this.activeRequestList;
 
     if (searchQuery) {
-        list = list.filter((item) => item.description.toLocaleLowerCase().includes(searchQuery) || item.title.toLocaleLowerCase().includes(searchQuery));
+      list = list.filter(
+        (item) =>
+          item.description.toLocaleLowerCase().includes(searchQuery) ||
+          item.title.toLocaleLowerCase().includes(searchQuery)
+      );
     }
 
     this.activeRequestList = list;
@@ -186,13 +229,13 @@ export class CustomerHomepageComponent implements OnInit, OnDestroy {
     if (this.currentPage < this.totalPages) {
       this.currentPage++;
     }
-  }
+  };
 
   previousPage = () => {
     if (this.currentPage > 1) {
       this.currentPage--;
     }
-  }
+  };
 
   goToPage(page: number) {
     if (page >= 1 && page <= this.totalPages) {
@@ -202,9 +245,60 @@ export class CustomerHomepageComponent implements OnInit, OnDestroy {
 
   navigateToNewRequest = () => {
     this.router.navigate(['nova-solicitacao']);
-  }
+  };
 
-  rightButtonProp: ButtonProps =  {text: '', icon: 'chevron_right', iconPosition: 'left' , color: 'white', size: 'small', textColor: 'black', extraClasses: 'border items-center text-center', onClick: this.nextPage}
-  leftButtonProp: ButtonProps =  {text: '', icon: 'chevron_left', iconPosition: 'left' , color: 'white', size: 'small', textColor: 'black', extraClasses: 'border items-center text-center', onClick: this.previousPage}
-  newRequestButtonProp: ButtonProps = {text: 'Nova Solicitação', color: 'primary-8', size: 'medium', textColor: 'white', hoverColor: 'primary-7' , onClick: this.navigateToNewRequest}
+  applyFilter = (filter: string, value?: string) => {
+    const key = filter === 'date' ? 'created_at' : filter;
+
+    const existingFilterIndex = this.activeFilters.findIndex(
+      (f) => f.filter === key
+    );
+
+    if (existingFilterIndex !== -1) {
+      if (!value || value === 'all' || value === '') {
+        this.activeFilters.splice(existingFilterIndex, 1);
+      } else {
+        this.activeFilters[existingFilterIndex].value = value;
+      }
+    } else {
+      if (value && value !== 'all' && value !== '') {
+        this.activeFilters.push({ filter: key, value });
+      }
+    }
+
+    this.activeRequestList = this.requestList.filter((item) => {
+      return this.activeFilters.every(
+        (f) => item[f.filter as keyof RequestItem] === f.value
+      );
+    });
+  };
+
+  rightButtonProp: ButtonProps = {
+    text: '',
+    icon: 'chevron_right',
+    iconPosition: 'left',
+    color: 'white',
+    size: 'small',
+    textColor: 'black',
+    extraClasses: 'border items-center text-center',
+    onClick: this.nextPage,
+  };
+  leftButtonProp: ButtonProps = {
+    text: '',
+    icon: 'chevron_left',
+    iconPosition: 'left',
+    color: 'white',
+    size: 'small',
+    textColor: 'black',
+    extraClasses: 'border items-center text-center',
+    onClick: this.previousPage,
+  };
+  newRequestButtonProp: ButtonProps = {
+    text: 'Nova Solicitação',
+    color: 'primary-8',
+    size: 'medium',
+    textColor: 'white',
+    hoverColor: 'primary-7',
+    onClick: this.navigateToNewRequest,
+  };
 }
