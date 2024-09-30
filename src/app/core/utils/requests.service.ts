5672c2e70694624b89fe6ca11d037b447358e730
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-09-29 21:07:50 -0300

    wip: implement mocks

--- src/app/core/utils/requests.service.ts
+++ src/app/core/utils/requests.service.ts
@@ -2,6 +2,8 @@ import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { RequestItem } from '../types/request-item';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +11,40 @@ import { environment } from '../../../environments/environment';
 export class RequestsService {
   private baseUrl: string = environment.baseUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  listRequests(): Observable<Object> {
-    return this.http.get(this.baseUrl + 'Requests.json');
-  }
+  listRequests(): Observable<RequestItem[]> {
+    return this.http.get<{ requests: any[] }>(this.baseUrl + 'requests.json').pipe(
+      map((response) => {
+        const requestMap: { [key: number]: { createdAt: string; status: string; requestDesc: string; defectDesc: string } } = {};
+  
+        response.requests.forEach((status) => {
+          const { requestId, statusDesc, dateTime, requestDesc, defectDesc } = status;
+  
+          if (!requestMap[requestId]) {
+            requestMap[requestId] = { 
+              createdAt: dateTime, 
+              status: statusDesc,
+              requestDesc,
+              defectDesc
+            };
+          } else {
+            if (new Date(dateTime) < new Date(requestMap[requestId].createdAt)) {
+              requestMap[requestId].createdAt = dateTime;
+            }
+            requestMap[requestId].status = statusDesc;
+          }
+        });
+  
+        return Object.entries(requestMap).map(([requestId, data]) => ({
+          id: Number(requestId),
+          title: data.requestDesc,
+          description: data.defectDesc,
+          status: data.status,
+          created_at: data.createdAt,
+          image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg=='
+        })) as RequestItem[];
+      })
+    );
+  }  
 }
