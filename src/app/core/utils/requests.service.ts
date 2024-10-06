905cba621c6532e4d786db7cd44e03792779b15e
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-10-06 17:47:07 -0300

    #33 docs: add comments do request service
    Co-authored-by: Gabriel-Troni troni.gabriel.10@gmail.com

--- src/app/core/utils/requests.service.ts
+++ src/app/core/utils/requests.service.ts
@@ -1,11 +1,10 @@
 import { Observable, lastValueFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { RequestStatus } from '../types/request-status';
 import { Request } from '../types/request';
 import { RequestItem } from '../types/request-item';
-import { RequestCategory } from '../types/request-category';
 
 @Injectable({
   providedIn: 'root',
@@ -15,19 +14,24 @@ export class RequestsService {
 
   constructor(private http: HttpClient) {}
   
+  // Método privado para obter a lista de requisições
   private getRequests(): Observable<Request[]> {
     return this.http.get<Request[]>(this.baseUrl + 'requests.json');
   }
   
+  // Método privado para obter os status das requisições
   private getRequestStatuses(): Observable<RequestStatus[]> {
     return this.http.get<RequestStatus[]>(this.baseUrl + 'requestStatuses.json');
   }
 
+  // Método assíncrono para listar as requisições com seus detalhes
   async listRequests(): Promise<RequestItem[]> {
     const requests: Request[] = await lastValueFrom(this.getRequests());
     const statuses: RequestStatus[] = await lastValueFrom(this.getRequestStatuses());
     
+    // Mapeia cada requisição para um objeto RequestItem
     const requestItems = requests.map((request: Request) => {
+      // Filtra os status que correspondem ao ID da requisição atual
       const statusList = statuses.filter((status) => status.requestId === request.requestId);
       const status = statusList[statusList.length - 1]?.category;
       return {
@@ -36,7 +40,7 @@ export class RequestsService {
         description: request.defectDesc,
         status: status,
         created_at: `${statusList[0]?.dateTime}`,
-        image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==',
+        image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAB0lEQVR42mP8/wcAAgAB/AmztHAAAAABJRU5ErkJggg==', // Imagem placeholder
       } as RequestItem;
     });
     
