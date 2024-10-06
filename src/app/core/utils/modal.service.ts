9a018f1cb75d34feae40f4581ac6d81a09c4670e
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-10-06 15:27:20 -0300

    feat: resolve modal return

--- src/app/core/utils/modal.service.ts
+++ src/app/core/utils/modal.service.ts
@@ -5,35 +5,36 @@ import { Observable } from 'rxjs';
 import { ModalType } from '../types/modal-type';
 import { ModalInputComponent } from '../components/modal/modal-input/modal-input.component';
 import { ModalConfirmComponent } from '../components/modal/modal-confirm/modal-confirm.component';
+import { ModalResponse } from '../types/modal-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
+  type!: ModalType;
 
-  constructor() { }
+  constructor() {}
 
-  open(host: ViewContainerRef, type: ModalType, data: { [key: string]: string }): Observable<string> {
+  open(host: ViewContainerRef, type: ModalType, data: { [key: string]: string }): Observable<ModalResponse> {
+    this.type = type;
     const injector = Injector.create({
       providers: [{ provide: 'data', useValue: data }]
     });
 
-    const lifeCycle: Observable<string> = new Observable((observer) => {
-      const modal = host.createComponent<ModalComponent>(this.modalFactory(type), {injector});
+    const lifeCycle: Observable<ModalResponse> = new Observable((observer) => {
+      const modal = host.createComponent<ModalComponent>(this.modalFactory(), {injector});
       const instance = modal.instance.open();
       instance.subscribe((value) => {
         modal.destroy();
-        if(value === 'close'){
-          observer.next("Abaixo o Capitalismo!!");//Código colocado somente para teste do Modal, mudar para algo mais util ou não.
-          observer.complete();
-        }
+        observer.next(value);
+        observer.complete();
       });
     });
     return lifeCycle;
   }
 
-  private modalFactory(type: ModalType): Type<ModalComponent>{
-    switch(type){
+  private modalFactory(): Type<ModalComponent>{
+    switch(this.type){
       case ModalType.INPUT:
         return ModalInputComponent;
       case ModalType.CONFIRM:
