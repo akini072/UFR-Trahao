b6e02c3cfef88e68de61c931d09a2e6785531f60
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-10-19 19:17:37 -0300

    #45 refactor: unified navbar using auth validation
    Co-authored-by: AlissonGSantos <alisson.gabriel@ufpr.br>

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../../../auth/utils/auth.service';
 
 interface NavItem {
   name: string;
@@ -12,36 +13,58 @@ interface NavItem {
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterModule],
+  providers: [AuthService],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-  @Input() isLogged: boolean = true;
+  isLogged: boolean;
+  user: string;
+  navItems: NavItem[];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private auth: AuthService) {
+    try {
+      const credentials = this.auth.getCurrentUser();
+      this.isLogged = true;
+      this.user = credentials.name;
+      switch (credentials.profile){
+        case 'Costumer':
+          this.navItems = [
+            { name: 'Início', route: '/customer', login: true }
+          ]
+          break;
+        case 'Employee':
+          this.navItems = [
+            { name: 'Início', route: '/pagina-funcionario', login: true },
+            { name: 'Solicitações', route: '/solicitacoes', login: true },
+            { name: 'Funcionários', route: '/funcionarios', login: true },
+            { name: 'Categorias', route: '/categorias', login: true },
+            { name: 'Relatório', route: '/relatorio-mensal', login: true }
+          ];
+          break;
+      }
+    } catch (Error) {
+      this.isLogged = false;
+      this.navItems = [
+        { name: 'Login', route: '/login' },
+        { name: 'Cadastro', route: '/cadastro' },
+      ];
+      this.user = '';
+    }
+  }
 
-  navContainer =
-    'flex flex-1 bg-primary-8 justify-between pt-2 md:h-20 md:pt-0'; //Nav
-  navDiv =
-    'flex flex-wrap h-full w-full items-center justify-center md:px-6 md:flex-nowrap md:justify-between'; //Div geral
-  navData =
-    'flex flex-wrap gap-2 align-middle justify-center items-center py-2 md:gap-4 md:flex-nowrap'; //Div logo+bem-vindo
-  navLogo = 'text-3xl font-serif text-white cursor-pointer'; //logo texto
-  navUserText = 'w-full text-secondary-0 text-center'; //bem-vindo
-  navList = 'flex h-full w-full align-center justify-center md:gap-2 md:w-auto'; //ul botões
-  navItem =
-    'flex h-12 w-full bg-primary-7 hover:bg-primary-6 cursor-pointer text-center align-middle items-center justify-center md:h-full p-px md:w-full md:flex-1 md:bg-primary-8 md:hover:bg-primary-7 md:p-0'; //li botão
-  navAnchor =
-    'text-white h-full w-full px-2 content-center shadow-md focus:ring-1 focus:ring-white focus:outline-offset-0 md:shadow-none'; //âncora botão
+  style = {
+    navContainer: 'flex flex-1 bg-primary-8 justify-between pt-2 md:h-20 md:pt-0', //Nav
+    navDiv: 'flex flex-wrap h-full w-full items-center justify-center md:px-6 md:flex-nowrap md:justify-between', //Div geral
+    navData: 'flex flex-wrap gap-2 align-middle justify-center items-center py-2 md:gap-4 md:flex-nowrap', //Div logo+bem-vindo
+    navLogo: 'text-3xl font-serif text-white cursor-pointer', //logo texto
+    navUserText: 'w-full text-secondary-0 text-center', //bem-vindo
+    navList: 'flex h-full w-full align-center justify-center md:gap-2 md:w-auto', //ul botões
+    navItem: 'flex h-12 w-full bg-primary-7 hover:bg-primary-6 cursor-pointer text-center align-middle items-center justify-center md:h-full p-px md:w-full md:flex-1 md:bg-primary-8 md:hover:bg-primary-7 md:p-0', //li botão
+    navAnchor: 'text-white h-full w-full px-2 content-center shadow-md focus:ring-1 focus:ring-white focus:outline-offset-0 md:shadow-none' //âncora botão
+  }
 
-  navItems: NavItem[] = [
-    { name: 'Login', route: '/login' },
-    { name: 'Cadastro', route: '/cadastro' },
-    { name: 'Home', route: '/home', login: true },
-    { name: 'Serviços', route: '/about', login: true },
-  ];
-
-  goToIndex(){
+  goToIndex() {
     this.router.navigate(['/']);
   }
 }
