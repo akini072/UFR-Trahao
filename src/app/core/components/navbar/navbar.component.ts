6ef6d48156935aaf10b6445ca3d20c090c6361fe
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-10-19 20:42:51 -0300

    #45 feat: implement logout method

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -53,14 +53,21 @@ export class NavbarComponent {
     }
   }
 
+  logout(){
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+
   style = {
     navContainer: 'flex flex-1 bg-primary-8 justify-between pt-2 md:h-20 md:pt-0', //Nav
     navDiv: 'flex flex-wrap h-full w-full items-center justify-center md:px-6 md:flex-nowrap md:justify-between', //Div geral
     navData: 'flex flex-wrap gap-2 align-middle justify-center items-center py-2 md:gap-4 md:flex-nowrap', //Div logo+bem-vindo
     navLogo: 'text-3xl font-serif text-white cursor-pointer', //logo texto
+    navRow: 'flex gap-2 items-center h-full', //Div botões
     navUserText: 'w-full text-secondary-0 text-center', //bem-vindo
     navList: 'flex h-full w-full align-center justify-center md:gap-2 md:w-auto', //ul botões
     navItem: 'flex h-12 w-full bg-primary-7 hover:bg-primary-6 cursor-pointer text-center align-middle items-center justify-center md:h-full p-px md:w-full md:flex-1 md:bg-primary-8 md:hover:bg-primary-7 md:p-0', //li botão
+    navButton: 'inline-block bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded', //botão
     navAnchor: 'text-white h-full w-full px-2 content-center shadow-md focus:ring-1 focus:ring-white focus:outline-offset-0 md:shadow-none' //âncora botão
   }
 
