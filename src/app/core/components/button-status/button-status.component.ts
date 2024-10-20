c22f33d1e29a06bfb5739e12135f837c8fe1c8bb
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-10-20 17:02:18 -0300

    #45 fix: button status action route

--- src/app/core/components/button-status/button-status.component.ts
+++ src/app/core/components/button-status/button-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { RequestCategory } from '../../types/request-category';
@@ -16,48 +16,53 @@ interface ButtonStatus {
   templateUrl: './button-status.component.html',
   styleUrl: './button-status.component.css',
 })
-export class ButtonStatusComponent {
+export class ButtonStatusComponent implements OnInit {
   @Input() status!: RequestCategory;
+  @Input() id!: number;
   @Input() color: string = 'bg-gray-300';
   @Input() displayDisabled: boolean = false;
+  buttonOptions: ButtonStatus[] = [];
+  style = {
+    cardButton: `text-xs p-2 cursor-pointer text-default-white font-semibold rounded`
+  }
 
-  cardButton = `text-xs p-2 cursor-pointer text-default-white font-semibold rounded`;
-
-  buttonOptions: ButtonStatus[] = [
-    {
-      type: 'open',
-      name: 'ABERTA',
-      route: '',
-      isDisabled: true,
-    },
-    { type: 'budgeted', name: 'APROVAR/REJEITAR', route: 'visualizar-servico/3' },
-    { type: 'rejected', name: 'RESGATAR', route: 'visualizar-servico/3' },
-    {
-      type: 'approved',
-      name: 'APROVADA',
-      route: '',
-      isDisabled: true,
-    },
-    {
-      type: 'redirected',
-      name: 'REDIRECIONADA',
-      route: '',
-      isDisabled: true,
-    },
-    { type: 'fixed', name: 'PAGAR', route: 'visualizar-servico/3' },
-    {
-      type: 'paid',
-      name: 'PAGO',
-      route: '',
-      isDisabled: true,
-    },
-    {
-      type: 'finalized',
-      name: 'FINALIZADA',
-      route: '',
-      isDisabled: true,
-    },
-  ];
+  ngOnInit() {
+     this.buttonOptions = [
+      {
+        type: 'open',
+        name: 'ABERTA',
+        route: '',
+        isDisabled: true,
+      },
+      { type: 'budgeted', name: 'APROVAR/REJEITAR', route: 'visualizar-servico/'+this.id },
+      { type: 'rejected', name: 'RESGATAR', route: 'visualizar-servico/'+this.id },
+      {
+        type: 'approved',
+        name: 'APROVADA',
+        route: '',
+        isDisabled: true,
+      },
+      {
+        type: 'redirected',
+        name: 'REDIRECIONADA',
+        route: '',
+        isDisabled: true,
+      },
+      { type: 'fixed', name: 'PAGAR', route: 'visualizar-servico/'+this.id },
+      {
+        type: 'paid',
+        name: 'PAGO',
+        route: '',
+        isDisabled: true,
+      },
+      {
+        type: 'finalized',
+        name: 'FINALIZADA',
+        route: '',
+        isDisabled: true,
+      },
+    ];
+  }
 
   selectButton(status: RequestCategory) {
     return this.buttonOptions.find((button) => button.type === status);
