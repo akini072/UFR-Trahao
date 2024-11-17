189d7379d16a738fbb50c725fb44c910879f52d3
rogerch0505@gmail.com <rogerch0505@gmail.com>
2024-11-16 23:41:07 -0300

    #66 feat:descriptografia do token

--- src/app/auth/utils/auth.service.ts
+++ src/app/auth/utils/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Credentials } from '../types/credentials';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   /*user = {
@@ -21,12 +22,12 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  private baseUrl = 'http://localhost:8080/service/v1/auth'
+  private baseUrl = 'http://localhost:8080/service/v1/auth';
 
   public getCurrentUser(): Credentials {
     const user = sessionStorage.getItem('credentials');
-    if (user == null ){
-      throw Error("Nenhuma sessão ativa");
+    if (user == null) {
+      throw Error('Nenhuma sessão ativa');
     }
     return JSON.parse(user);
   }
@@ -45,9 +46,34 @@ export class AuthService {
     throw Error("Usuário ou senha inválidos");
   }*/
 
-  login(email: string,password: string): Observable<Credentials>{
-    const params= { email, password };
-    return this.http.post<Credentials>(`${this.baseUrl}/login`,null, {params});
+  login(email: string, password: string): Observable<Credentials> {
+    const params = { email, password };
+    return this.http
+      .post<Credentials>(`${this.baseUrl}/login`, null, {
+        params,
+      })
+      .pipe(
+        map((response) => {
+          // Salvar o token na sessão
+          const token = (response as any).token; // Supondo que o token seja retornado no campo 'token'
+          sessionStorage.setItem('credentials', JSON.stringify(response));
+
+          // Decodificar o token JWT
+          const decodedToken = this.decodeToken(token);
+          console.log('Token decodificado:', decodedToken);
+
+          return response;
+        })
+      );
+  }
+
+  private decodeToken(token: string): any {
+    try {
+      return jwtDecode(token); // Decodifica o token JWT
+    } catch (error) {
+      console.error('Erro ao decodificar o token:', error);
+      return null;
+    }
   }
 
   logout() {
